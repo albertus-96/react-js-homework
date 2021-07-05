f7d8d839107543896d946d86ab6f0af170272af1
Albertus Eko Suryanto <AlbertusEko.Suryanto@mitrais.com>
2021-07-05 10:26:21 +0700

    add edit function

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,14 +2,31 @@ import { useEffect, useState } from "react";
 
 function Profile(props) {
   const [profile, setProfile] = useState(props);
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     setProfile(props);
   }, [props]);
 
+  const handleChangeName = (e) => {
+    setProfile({ ...profile, name: e });
+  };
+
+  const handleChangeAge = (e) => {
+    setProfile({ ...profile, age: parseInt(e) });
+  };
+
+  const handleChangeEmail = (e) => {
+    setProfile({ ...profile, email: e });
+  };
+
   const removeProfile = (e) => {
     props.remove(profile);
     e.preventDefault();
+  };
+
+  const editProfile = (e) => {
+    setIsEditing((isEditing) => !isEditing);
   };
 
   return (
@@ -24,26 +41,72 @@ function Profile(props) {
             />
           </div>
           <div className="ml-3">
-            <p className="text-gray-900 whitespace-no-wrap">{profile.name}</p>
+            {isEditing ? (
+              <p>
+                <input
+                  type="text"
+                  placeholder="Enter name here..."
+                  value={profile.name}
+                  onChange={(e) => handleChangeName(e.target.value)}
+                  className="bg-gray-200 max-w-full focus:outline-none text-gray-700"
+                />
+              </p>
+            ) : (
+              <p className="text-gray-900 whitespace-no-wrap">{profile.name}</p>
+            )}
           </div>
         </div>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 whitespace-no-wrap">{profile.age}</p>
+        {isEditing ? (
+          <input
+            type="number"
+            value={profile.age}
+            onChange={(e) => handleChangeAge(e.target.value)}
+            placeholder="Enter age here..."
+            className="bg-gray-300 max-w-full focus:outline-none text-gray-700"
+          />
+        ) : (
+          <p className="text-gray-900 whitespace-no-wrap">{profile.age}</p>
+        )}
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 whitespace-no-wrap">{profile.email}</p>
+        {isEditing ? (
+          <input
+            type="text"
+            placeholder="Enter email here..."
+            value={profile.email}
+            onChange={(e) => handleChangeEmail(e.target.value)}
+            className="bg-gray-300 max-w-full focus:outline-none text-gray-700"
+          />
+        ) : (
+          <p className="text-gray-900 whitespace-no-wrap">{profile.email}</p>
+        )}
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        {/* <span className="relative inline-block px-3 py-1 font-semibold text-gray-800 leading-tight mr-4">
-          <span
-            aria-hidden
-            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-          ></span>
-          <span className="relative cursor-pointer" onClick={editProfile}>
-            Edit
-          </span>
-        </span> */}
+        <span className="relative inline-block px-3 py-1 font-semibold text-gray-800 leading-tight mr-4">
+          {isEditing ? (
+            <span>
+              <span
+                aria-hidden
+                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+              ></span>
+              <span className="relative cursor-pointer" onClick={editProfile}>
+                Done
+              </span>
+            </span>
+          ) : (
+            <span>
+              <span
+                aria-hidden
+                className="absolute inset-0 bg-blue-200 opacity-50 rounded-full"
+              ></span>
+              <span className="relative cursor-pointer" onClick={editProfile}>
+                Edit
+              </span>
+            </span>
+          )}
+        </span>
         <span className="relative inline-block px-3 py-1 font-semibold text-gray-800 leading-tight">
           <span
             aria-hidden
